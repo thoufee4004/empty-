62f00a61364f5b417ebc74c7b6975ad28799ad7e
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2018-09-03 19:40:22 +0530

    Create odd or even.py

---
+++ odd or even.py
@@ -0,0 +1,11 @@
+print("check whether a number is odd or even")
+a=int(input("enter the number"))
+print(a)
+if(a<0):
+    print('the given input is not valid')
+elif(a==0):
+    print('the given input is zero')
+elif(a%2!=1):
+    print('the given number',+a,'is even')
+elif(a%2==1):
+ print('the given number',+a,'is odd')
