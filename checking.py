34a5c29df8b425d5fd9c07b39aee744bf08ab37c
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2018-09-03 18:46:51 +0530

    Create checking.py

---
+++ checking.py
@@ -0,0 +1,8 @@
+print("check the given number is positive or negative or zero")
+a=input("enter the number")
+if(a<0):
+    print('the given',+a,"is negative number")
+elif(a==0):
+    print('the given',a,"is equal to zero")
+else:
+    print('the given',a,"is positive number")
