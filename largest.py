3912485aa119d7c88e228af4c4f8016f4e0d6440
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2019-02-07 12:14:19 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,9 @@
-t1=int(raw_input())
+t11=int(raw_input())
 t2=int(raw_input())
 t3=int(raw_input())
-if((t1>=t2) and (t1>=t3)):
-  print(t1)
-elif((t2>=t1) and (t2>=t3)):
+if((t11>=t2) and (t11>=t3)):
+  print(t11)
+elif((t2>=t11) and (t2>=t3)):
   print(t2)
 else:
   print(t3)
