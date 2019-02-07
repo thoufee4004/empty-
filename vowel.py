853a44be90c0cf84b0ff48da9692001784ecddb0
B.Al mohammed thoufeek <42872880+thoufee4004@users.noreply.github.com>
2019-02-07 11:05:18 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,10 +1,6 @@
 list =['a','e','i','o','u','A','E','I','O','U']
-a=raw_input("enter the input")
-print(a)
-if a.isdigit():
- print('the input',a,"is a integer input")
+a=raw_input()
+if(a in list):
+  print('Vowel')
 else:
- if(a in list):
-  print('the input',a,'is a vowel letter')
- else:
-    print('the input',a,'is not a vowel')
+ print('invalid')
